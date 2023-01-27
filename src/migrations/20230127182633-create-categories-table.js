9a4c9da6af10c7e8a4438861377300eacba71bd7
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-01-27 15:41:04 -0300

    adiciona migration que cria a tabela categories

---
+++ src/migrations/20230127182633-create-categories-table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
