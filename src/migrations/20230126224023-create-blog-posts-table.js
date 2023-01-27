9baf72859871f28ceea9d08858a341f29852bb3e
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-01-27 14:45:05 -0300

    corrige erro de type na migration que cria tabela blog_posts

--- src/migrations/20230126224023-create-blog-posts-table.js
+++ src/migrations/20230126224023-create-blog-posts-table.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.DATE,
       },
       updated: {
-        typeof: Sequelize.DATE,
+        type: Sequelize.DATE,
       },
       userId: {
         field: 'user_id',
