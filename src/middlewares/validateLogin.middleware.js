74e885ff76cd5e3725baceff58b0ee197a51edae
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-01-31 00:07:54 -0300

    adiciona middleware de validacao da rota login

---
+++ src/middlewares/validateLogin.middleware.js
@@ -0,0 +1,10 @@
+const { ErrorGenerator, types } = require('../utils/errorSettings');
+
+module.exports = (req, _res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ErrorGenerator(types.BAD_REQUEST, 'Some required fields are missing'); 
+  }
+
+  return next();
+};
