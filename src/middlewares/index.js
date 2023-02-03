1f1efa5d673d3f94342e90f7340f1ff09b6d320d
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-02-03 19:43:56 -0300

    adiciona middleware de validacao de token

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const errorHandlerMiddleware = require('./errorHandler.middleware');
 const validateLoginMiddleware = require('./validateLogin.middleware');
 const validateAddUserMiddleware = require('./validateAddUser.middleware');
+const validateTokenMiddleware = require('./validateToken.middleware');
 
 module.exports = {
   errorHandlerMiddleware,
   validateLoginMiddleware,
   validateAddUserMiddleware,
+  validateTokenMiddleware,
 };
---
+++ src/middlewares/validateToken.middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('../utils/JWT');
+const { ErrorGenerator, types } = require('../utils/errorSettings');
+const { User } = require('../models');
+
+module.exports = async (req, _res, next) => {
+  try {
+    const token = req.header('Authorization');
+    if (!token) throw new ErrorGenerator(types.UNAUTHENTICATED, 'Token not found');
+    const result = jwt.decodeToken(token);
+    if (result.error) throw new ErrorGenerator(types.UNAUTHENTICATED, 'Expired or invalid token');
+    const user = await User.findOne({ where: { email: result.data.email } });
+    if (!user) throw new ErrorGenerator(types.UNAUTHENTICATED, 'Invalid token');
+    return next();
+  } catch (error) {
+    next(error);
+  }
+};
