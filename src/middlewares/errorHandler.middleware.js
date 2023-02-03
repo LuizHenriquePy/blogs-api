71c702c30b51e0278517a8bd0e1447473c2ac5ad
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-02-02 21:00:02 -0300

    adiciona console.log no middleware errorHandler

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -2,5 +2,6 @@ module.exports = (error, _req, res, _next) => {
   if (error.type) {
     return res.status(error.type).json({ message: error.message });
   }
+  console.log(error.message);
   return res.status(500).json({ message: 'internal server error' });
 };
