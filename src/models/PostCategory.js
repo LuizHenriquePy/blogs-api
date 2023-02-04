0dda4417673a0b2cd30ba4707d34e7bdd1463060
Luiz Henrique Mendes da Silva <luizhenriquemendes123123@gmail.com>
2023-02-04 18:12:44 -0300

    cria model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,42 @@
+const postCategoryModel = (sequelize, dataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+  {
+    postId: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    }
+  },
+  {
+    underscored: true,
+    tableName: 'categories',
+    timestamps: false,
+  }
+  );
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost,
+      {
+        as: 'posts',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId'
+      });
+    models.BlogPost.belongsToMany(models.Category,
+      {
+        as: 'Categories',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId'
+      });
+  }
+
+  return PostCategory;
+}
+
+module.exports = postCategoryModel;
